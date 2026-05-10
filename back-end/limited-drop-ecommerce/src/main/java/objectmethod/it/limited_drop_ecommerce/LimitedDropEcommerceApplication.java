64a2bdab2f22ff3920e64db44276db87408d95cc
Moaaz980 <moaaz.bahnassawi@itsrizzoli.it>
2026-05-10 14:09:44 +0200

    configurazioni

---
+++ back-end/limited-drop-ecommerce/src/main/java/objectmethod/it/limited_drop_ecommerce/LimitedDropEcommerceApplication.java
@@ -0,0 +1,11 @@
+package objectmethod.it.limited_drop_ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedDropEcommerceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedDropEcommerceApplication.class, args);
+	}
+}
---
+++ back-end/limited-drop-ecommerce/src/test/java/objectmethod/it/limited_drop_ecommerce/LimitedDropEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package objectmethod.it.limited_drop_ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedDropEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
